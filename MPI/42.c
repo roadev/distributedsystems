8cf2d0062c9c75c1b0ea4934c343a7436c6f1de2
Juan Roa <jdroa92@gmail.com>
2016-06-06 23:42:28 -0500

    42.c added

--- MPI/42.c
+++ MPI/42.c
@@ -1,8 +1,3 @@
-/*
-* Este programa genera un deadlock
-*/
-
-
 #include <stdio.h>
 #include "mpi.h"
 
@@ -18,13 +13,13 @@ int main(int argc,char *argv[]){
 	if (rank == 0) {
 	  dest = 1;
 	  source = 1;
-	  MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-	  MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+		MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
 	}else if (rank == 1) {
 	  dest = 0;
 	  source = 0;
-	  MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
 	  MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
 	 }
 
 	MPI_Get_count(&Stat, MPI_CHAR, &count);
