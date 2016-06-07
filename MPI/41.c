144f3c1291ff3953d192c678dde3b6e399a9f571
Juan Roa <jdroa92@gmail.com>
2016-06-06 23:38:49 -0500

    Adding 41.c

--- MPI/41.c
+++ MPI/41.c
@@ -13,8 +13,8 @@ int main(int argc,char *argv[]){
 	if (rank == 0) {
 	  dest = 1;
 	  source = 1;
-	  MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
 	  MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+		MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
 	}else if (rank == 1) {
 	  dest = 0;
 	  source = 0;
